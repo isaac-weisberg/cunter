fbf8d08f76074679136a2e070cacc5692f0cddc1
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-05-20 21:54:27 +0700

    Remote read function that relates to cundefs

--- rw.go
+++ rw.go
@@ -3,24 +3,7 @@ package cunter
 import (
 	"encoding/json"
 	"io/ioutil"
-
-	"github.com/isaac-weisberg/cundef"
 )
-
-func read(path string) (*cundef.Project, error) {
-	var error error
-	var data []byte
-	data, error = ioutil.ReadFile(path)
-	if error != nil {
-		return nil, error
-	}
-	var project = cundef.Project{}
-	error = json.Unmarshal(data, project)
-	if error != nil {
-		return nil, error
-	}
-	return &project, nil
-}
 
 func export(path string, object interface{}) error {
 	var error error
